268119b9f07c10a2a5b355e8ca17c54652eadd09
haku <tridan243@gmail.com>
2021-12-12 22:44:02 +0700

    push bai6,7

---
+++ HocPython/bai6/6-3.py
@@ -0,0 +1,5 @@
+
+c = complex(2, 5)
+print(c)
+print(c.real)
+print(c.imag)
---
+++ HocPython/bai7/7-1.py
@@ -0,0 +1,10 @@
+strHowKteam = 'Free education'
+print(strHowKteam)
+print(type(strHowKteam))
+
+strHowKteam = '''
+HowKteam.com
+Free ducation
+Share tobe better
+'''
+print(strHowKteam)
