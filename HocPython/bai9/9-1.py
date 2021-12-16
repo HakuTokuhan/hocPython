526336e0f2ba2b87d50eb39e776cb264b94e1999
haku <tridan243@gmail.com>
2021-12-17 00:19:22 +0700

    push bai9

---
+++ HocPython/bai9/9-1.py
@@ -0,0 +1,16 @@
+a = 'My team is %s'%('Kteam')
+print(a)
+
+a = 'My team is %s %s years old' %('1', '2')
+print(a)
+
+s = '%s %s'
+result = s %('1', '2')
+print(result)
+
+f = '%.3f' %(3.9999)
+print(f)
+
+Kteam = 'Kteam'
+result = f'{Kteam} - Free education'
+print(result)
