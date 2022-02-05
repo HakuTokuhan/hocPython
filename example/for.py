dff6e122f9d9f94a119faf7d04e7c273b9d5f2b1
haku <tridan243@gmail.com>
2022-02-06 00:28:28 +0700

    push bai tren lop

---
+++ example/for.py
@@ -0,0 +1,14 @@
+for x in range (3, 7) :
+    print("Value of x = ", x)
+for x in range(6):
+    print(x)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        continue
+print(x)
+
+names =["Tom","Jerry", "Donald"]
+for index in range(len(names)):
+    print("Name = ", names[index])
